a4d814b95963f0584546cf972059a89e47778b8c
Malik <malik_ali1998@hotmail.co.uk>
2020-10-17 20:05:35 +0100

    Update BasicCalculatorPython.py
    taking Integer input, change to float for decimal input

--- BasicCalculatorPython.py
+++ BasicCalculatorPython.py
@@ -1,2 +1,4 @@
-name = input("Enter your Name: ")
-print ("Hello " + name)
+num1 = int(input("Enter First Number: "))#takeing only int input
+num2 = int(input("Enter Second Number: "))
+result = num1 + num2 #or i could change the varibles to int e.g. int(num1) + int(num2)
+print(result)
