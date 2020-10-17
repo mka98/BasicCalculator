7791110c6dd1c0de000614e6112d150cb202a2a2
Malik <malik_ali1998@hotmail.co.uk>
2020-10-17 20:49:38 +0100

    Update BasicCalculatorPython.py
    adding operators

--- BasicCalculatorPython.py
+++ BasicCalculatorPython.py
@@ -1,4 +1,21 @@
 num1 = int(input("Enter First Number: "))#takeing only int input
+op = input("Enter Operater + - / *: ")
 num2 = int(input("Enter Second Number: "))
-result = num1 + num2 #or i could change the varibles to int e.g. int(num1) + int(num2)
-print(result)
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else:
+    print("Invalid Operator") 
+    
+    
+    
+    
+    
+#result = num1 + num2 #or i could change the varibles to int e.g. int(num1) + int(num2)
+#print(result)
